b2bd58f367d7829f7bdffd28d71b2b77bd50f95f
manfromny <ebacikoz@gmail.com>
2023-01-06 21:10:46 -0500

    added description textarea to DataForm

--- src/Components/ListingForm/DataForm.jsx
+++ src/Components/ListingForm/DataForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, Fragment } from "react";
 import { CloseIcon, LeftPointArrow, RightPointArrow } from "../UI/Icons";
 
 const defaultFormData = {
+  description: "",
   condition: "",
   year: "",
   make: "",
@@ -24,13 +25,14 @@ const defaultFormData = {
 
 const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
   const style =
-    "px-2 py-2 outline-none w-[90%] mx-auto my-auto rounded-xl text-center bg-slate-200 placeholder:text-slate-400";
+    "focus:bg-slate-100 shadow-sm px-2 py-2 outline-none w-[90%] mx-auto my-auto rounded-xl text-center bg-slate-200 placeholder:text-slate-400";
 
   const [formData, setFormData] = useState(defaultFormData);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(0);
   const [edited, setEdited] = useState(false);
 
   const {
+    description,
     condition,
     year,
     make,
@@ -71,10 +73,27 @@ const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
 
   return (
     <form
-      className="mx-auto items-center text-center min-w-[16rem] max-w-[24rem] w-[90%] lg:[35%] min-h-[28rem] h-[28rem]"
+      className="py-4 flex flex-col justify-around space-y-2 mx-auto items-center text-center min-w-[16rem] max-w-[24rem] w-[100%] lg:[35%] min-h-[30rem] max-h-[36rem]"
       onSubmit={formSubmitHandler}
     >
-      <div className="flex flex-col justify-between max-w-[25rem] xl:max-w-[40rem] min-h-[90%] mx-auto py-4">
+      <h1 className="text-2xl font-light bg-slate-100 px-3 py-2 rounded-xl">
+        Details
+      </h1>
+      <div className="w-[90%] flex flex-col justify-between space-y-4 max-w-[25rem] xl:max-w-[40rem] mx-auto py-2">
+        {pageNumber === 0 && (
+          <div name="description" className="flex w-[90%] h-[20rem] mx-auto">
+            <textarea
+              type="textarea"
+              className="text-lg shadow-md mx-auto my-auto rounded-xl bg-slate-100 w-full h-full resize-none outline-none px-2 py-2 text-slate-600"
+              placeholder="Tell people about your car."
+              name="description"
+              value={description}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        )}
+
         {pageNumber === 1 && (
           <Fragment>
             <input
@@ -242,11 +261,11 @@ const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
           </Fragment>
         )}
       </div>
-      <div className="flex min-h-[10%] items-center">
-        {pageNumber > 1 && (
+      <div className="flex items-center w-full">
+        {pageNumber > 0 && (
           <button
             type="button"
-            className="flex space-x-2 bg-gray-200 px-3 py-1 text-blue-600 mx-auto my-auto rounded-xl"
+            className="shadow-sm flex space-x-2 bg-gray-100 px-3 py-2 text-blue-600 mx-auto my-auto rounded-xl active:opacity-50"
             onClick={() => setPageNumber(pageNumber - 1)}
           >
             <LeftPointArrow />
@@ -256,7 +275,7 @@ const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
         {edited && (
           <button
             type="button"
-            className="flex space-x-2 bg-red-500 px-3 py-1 text-slate-50 mx-auto my-auto rounded-xl"
+            className="shadow-sm flex space-x-2 bg-red-500 px-3 py-1 text-slate-50 mx-auto my-auto rounded-xl active:opacity-50"
             onClick={handleClearForm}
           >
             <CloseIcon size={`w-6 h-6`} />
@@ -266,7 +285,7 @@ const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
         {pageNumber < 3 && (
           <button
             type="button"
-            className="flex space-x-2 bg-gray-200 px-3 py-1 text-blue-600 mx-auto my-auto rounded-xl"
+            className="shadow-sm flex space-x-2 bg-gray-100 px-3 py-2 text-blue-600 mx-auto my-auto rounded-xl active:opacity-50"
             onClick={() => setPageNumber(pageNumber + 1)}
           >
             Next
@@ -276,7 +295,7 @@ const DataForm = ({ handleSubmit, uploadedImageKeys }) => {
         {pageNumber === 3 && (
           <button
             type="submit"
-            className="text-md mx-auto px-5 py-1 bg-blue-500 text-slate-100 rounded-2xl 
+            className="shadow-sm text-md mx-auto px-5 py-2 bg-blue-500 text-slate-100 rounded-2xl 
           hover:bg-yellow-500 hover:text-white hover:scale-110 tranition duration-150 "
           >
             Submit
--- src/Components/ListingForm/ListingContainer.jsx
+++ src/Components/ListingForm/ListingContainer.jsx
@@ -35,19 +35,19 @@ const ListingContainer = () => {
   return (
     <div
       className="flex flex-col justify-between items-center space-y-4 my-auto 
-       py-4 w-full  max-w-5xl md:px-2"
+       py-4 w-full max-w-5xl md:px-2"
     >
-      <h1 className="text-2xl text-center h-[5%] font-light">
+      <h1 className="text-2xl text-center h-[5%] font-light bg-slate-100 px-3 py-2 rounded-xl items-center">
         Create a Listing
       </h1>
-      <div className="w-[100%] h-[100%] flex flex-col lg:flex-row justify-between items-center ">
+      <div className="w-[100%] h-[100%] flex flex-col lg:flex-row justify-between space-y-2 items-center ">
         <div
           className="lg:w-[65%] w-[100%] sm:w-[70%] max-w-[100%] flex flex-col
-        rounded-xl lg:max-h-[36rem] border-4 "
+        md:rounded-xl lg:max-h-[36rem] border-4 "
         >
           <ImageContainer uploadedImageKeys={uploadedImageKeys} />
         </div>
-        <div className="lg:min-w-[35%] min-w-[50%] flex items-center ">
+        <div className="lg:min-w-[35%] min-w-[90%] flex items-center">
           <DataForm uploadedImageKeys={uploadedImageKeys} />
         </div>
       </div>
